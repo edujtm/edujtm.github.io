ab40286d307dd4aa1bd290ab80e0f94d0550c489
Your Name <eduzemacedo@hotmail.com>
2020-10-28 01:30:26 -0300

    Refactoring lottie animations

---
+++ src/components/LottieFile.js
@@ -0,0 +1,31 @@
+import React, { createRef, useEffect } from "react";
+
+import lottie from "lottie-web";
+
+
+const LottieFile = ({ className, animationData, autoplay }) => {
+    const animationContainer = createRef(); 
+
+    useEffect(() => {
+        const animation = lottie.loadAnimation({
+            container: animationContainer.current,  
+            renderer: "svg",
+            loop: true,
+            autoplay,
+            animationData,
+        });
+
+        return () => { animation.destroy() };
+    }, [animationContainer]);
+
+    return (
+        <div className={className} ref={animationContainer}>
+        </div> 
+    )
+};
+
+LottieFile.defaultProps = {
+    autoplay: false,
+}
+
+export default LottieFile;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-import React, { useEffect, createRef } from "react"
+import React from "react"
 import { Link } from 'gatsby';
-import lottie from 'lottie-web';
 
 import Layout from "../components/Layout";
 import Head from "../components/Head";
+import LottieFile from "../components/LottieFile";
 
 import indexStyles from './index.module.scss';
 
@@ -12,41 +12,23 @@ import campDude from  "../animations/camp-guy.json";
 import campFire from "../animations/fire-crackling-in-a-campfire.json";
 
 const IndexPage = () => {
-  let dudeContainer = createRef();
-  let campContainer = createRef();
-
-  useEffect(() => {
-    const dudeAnim = lottie.loadAnimation({
-      container: dudeContainer.current,
-      renderer: "svg",
-      loop: true,
-      autoplay: true,
-      animationData: campDude
-    });
-
-    const campAnim = lottie.loadAnimation({
-      container: campContainer.current,
-      renderer: "svg",
-      loop: true,
-      autoplay: true,
-      animationData: campFire
-    });
-
-    return () => {
-      dudeAnim.destroy();
-      campAnim.destroy();
-    };
-  }, [dudeContainer, campContainer]);
-
-  return (
+    return (
     <Layout>
       <Head title="Home" /> 
       <h1>Hello.</h1>
       <h2>I'm <Link to="/about">Eduardo Macedo</Link>, this is where I sit down and share my thoughts about Coding topics. </h2>
       <h2>Hopefully I'll be able to share useful information to the world, the same way it did for me. </h2>
       <div className={indexStyles.animationContainer}>
-        <div className={indexStyles.dudeAnimation} ref={dudeContainer} />
-        <div className={indexStyles.campFireAnimation} ref={campContainer} />
+        <LottieFile 
+          className={indexStyles.dudeAnimation}
+          animationData={campDude}
+          autoplay
+        />
+        <LottieFile 
+          className={indexStyles.campFireAnimation}
+          animationData={campFire}
+          autoplay
+        />
       </div>
     </Layout>
   )
